b888aecea2b5fc08ab42e265983e9c3e3f915427
EOS Classic <info@eos-classic.io>
2018-09-14 21:32:07 +0900

    Several cleanup
    + Changed default goreli genesis block to match with Parity ( Still WIP, need fix on Aura engine )
    + Removed build-related files

--- core/types/block.go
+++ core/types/block.go
@@ -19,7 +19,6 @@ package types
 
 import (
 	"encoding/binary"
-	"github.com/ethereum/go-ethereum/params"
 	"io"
 	"math/big"
 	"sort"
@@ -30,6 +29,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
@@ -66,23 +66,6 @@ func (n *BlockNonce) UnmarshalText(input []byte) error {
 }
 
 //go:generate gencodec -type Header -field-override headerMarshaling -out gen_header_json.go
-type AuraHeader struct {
-	ParentHash  common.Hash    `json:"parentHash"       gencodec:"required"`
-	UncleHash   common.Hash    `json:"sha3Uncles"       gencodec:"required"`
-	Coinbase    common.Address `json:"miner"            gencodec:"required"`
-	Root        common.Hash    `json:"stateRoot"        gencodec:"required"`
-	TxHash      common.Hash    `json:"transactionsRoot" gencodec:"required"`
-	ReceiptHash common.Hash    `json:"receiptsRoot"     gencodec:"required"`
-	Bloom       Bloom          `json:"logsBloom"        gencodec:"required"`
-	Difficulty  *big.Int       `json:"difficulty"       gencodec:"required"`
-	Number      *big.Int       `json:"number"           gencodec:"required"`
-	GasLimit    uint64         `json:"gasLimit"         gencodec:"required"`
-	GasUsed     uint64         `json:"gasUsed"          gencodec:"required"`
-	Time        *big.Int       `json:"timestamp"        gencodec:"required"`
-	Extra       []byte         `json:"extraData"        gencodec:"required"`
-	Signature1  []byte         `json:"signature1,omitempty"`
-	Signature2  []byte         `json:"signature2,omitempty"`
-}
 
 // Header represents a block header in the Ethereum blockchain.
 type Header struct {
@@ -102,6 +85,25 @@ type Header struct {
 	MixDigest   common.Hash       `json:"mixHash"          gencodec:"required"`
 	Nonce       BlockNonce        `json:"nonce"            gencodec:"required"`
 	Signatures  params.Signatures `json:"signature,omitempty"`
+}
+
+// AuraHeader represents a block header in the Aura blockchain.
+type AuraHeader struct {
+	ParentHash  common.Hash    `json:"parentHash"       gencodec:"required"`
+	UncleHash   common.Hash    `json:"sha3Uncles"       gencodec:"required"`
+	Coinbase    common.Address `json:"miner"            gencodec:"required"`
+	Root        common.Hash    `json:"stateRoot"        gencodec:"required"`
+	TxHash      common.Hash    `json:"transactionsRoot" gencodec:"required"`
+	ReceiptHash common.Hash    `json:"receiptsRoot"     gencodec:"required"`
+	Bloom       Bloom          `json:"logsBloom"        gencodec:"required"`
+	Difficulty  *big.Int       `json:"difficulty"       gencodec:"required"`
+	Number      *big.Int       `json:"number"           gencodec:"required"`
+	GasLimit    uint64         `json:"gasLimit"         gencodec:"required"`
+	GasUsed     uint64         `json:"gasUsed"          gencodec:"required"`
+	Time        *big.Int       `json:"timestamp"        gencodec:"required"`
+	Extra       []byte         `json:"extraData"        gencodec:"required"`
+	Signature1  []byte         `json:"signature1,omitempty"`
+	Signature2  []byte         `json:"signature2,omitempty"`
 }
 
 func (h *Header) MixDig() common.Hash {
